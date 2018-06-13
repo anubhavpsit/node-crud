53bd70c907d22bec45cb839a7726388441b69c0b
anubhav <anubhav@octane.in>
2018-06-13 12:27:33 +0530

    get history data

--- routes/getHistory.js
+++ routes/getHistory.js
@@ -4,6 +4,37 @@ var path = require('path');
 var fs = require('fs');
 var key = "7cab2267ab2cf19c4b6eaa0ce02d9f0e";
 
+// make Promise version of fs.readdir()
+fs.readdirAsync = function(dirname) {
+    return new Promise(function(resolve, reject) {
+        fs.readdir(dirname, function(err, filenames){
+            if (err) 
+                reject(err); 
+            else 
+                resolve(filenames);
+        });
+    });
+};
+
+// make Promise version of fs.readFile()
+fs.readFileAsync = function(filename, enc) {
+    return new Promise(function(resolve, reject) {
+        fs.readFile(filename, enc, function(err, data){
+            if (err) 
+                reject(err); 
+            else
+                resolve(data);
+        });
+    });
+};
+
+var getFile = function(dirPath) {
+	return function(filename) { return fs.readFileAsync(dirPath +"/"+ filename, 'utf8'); };
+    //return function(filename) { return fs.readFileAsync(dirPath +"/"+ filename); };
+};
+
+
+
 router.post('/history', function (req, res) {
 
 	res.writeHead(200, {'Content-Type': 'text/json'});
@@ -34,87 +65,36 @@ router.post('/history', function (req, res) {
 	} else{
 		console.log("Request object validated");
 	    var todayDate = new Date().toISOString().slice(0,10);
-	    var dirPath = "history/"+todayDate+"/"+req.body.userid;
-	    console.dir(dirPath);
-	    
-	    var fileNames = [];
-		readFiles('dirPath', (name, ext, path) => {
-		  console.log(`file name is: ${name}`);
-		  console.log(`file extension is: ${ext}`);
-		  console.log(`file path is: ${path}`);
-		});
-//	    var fileNames = getFileNames(dirPath);
-	    //var files = fs.readdir(dirPath);
-		// fs.readdir(dirPath, (err, files) => {
-		//   files.forEach(file => {
-		// 	console.dir(file);
-		//     fileNames.push(file);
-		//   });
-		// });
+	    var dirPath = "history/"+req.body.date+"/"+req.body.userid;
 
-		console.dir(fileNames);
-
-	    //Check if user's today dir is created or not if not create it
-	  //   if(!mkdirpath(dirPath)) {
-			// console.log("Creating direcotry to save file");
-			// mkdirpath(dirPath);
-	  //   }
-		
-	    // Directory created show some engineering and add the data now :)
-		// var filePath = "history/"+todayDate+"/"+req.body.userid+"/"+req.body.time+".txt";
-		// fs.writeFile(filePath, req.body.data, function(err) {
-		// 	console.log("Writing file in directory");
-		//     if(err) {
-		//     	console.log("unable to save file");
-		//         console.log(err);
-		// 		var data = new Object();
-		// 		data.success = false;
-		// 		data.status_code = 200;
-		// 		data.message = "Unable to save file";
-		// 		res.end(JSON.stringify(data));
-		//     }
-
+	    // Check if user directory exists or not
+		var exists = fs.existsSync(dirPath);		
+		if(exists) {
+			var summaryFiles = [];
+			//http://www.yaoyuyang.com/2017/01/20/nodejs-batch-file-processing.html
+			fs.readdirAsync(dirPath).then(function (filenames) {
+			    return Promise.all(filenames.map(getFile(dirPath)));
+			}).then(function (files) {
+			    files.forEach(function(file) {
+			    	summaryFiles.push(JSON.parse(file));
+			    });
+				var data = new Object();
+				data.success = true;
+				data.status_code = 200;
+				data.message = "File data accessed successfully";
+				data.data = summaryFiles;
+				res.end(JSON.stringify(data));
+			});
+		} else {
 			var data = new Object();
 			data.success = true;
 			data.status_code = 200;
-			data.message = "File saved successfully";
-			res.end(JSON.stringify(data));
-		// });
+			data.message = "File not found";
+			res.end(JSON.stringify(data));			
+		}
 	}
 })
 
-
-function getFileNames(dirname) {
-	console.dir("A");
-	var fileNames = [];
-  	fs.readdirsync(dirname, function(err, filenames) {
-    filenames.forEach(function(filename) {
-    	console.dir("B");
-    	fileNames.push(filename);
-    });
-  });
-
-  	return fileNames;
-
-}
-
-function readFiles(dirPath, processFile) {
-  // read directory
-  FS.readdir(dirPath, (err, fileNames) => {
-    if (err) throw err;
-    // loop through files
-    fileNames.forEach((fileName) => {
-      // get current file name
-      let name = PATH.parse(fileName).name;
-      // get current file extension
-      let ext = PATH.parse(fileName).ext;
-      // get current file path
-      let path = PATH.resolve(dirPath, fileName);
-      // callback, do something with the file
-      //processFile(name, ext, path);
-    });
-  });
-}
 
 function validateHistoryObject(data) 
 {
@@ -143,19 +123,19 @@ function validateHistoryObject(data)
 			}
 		}
 
-		if(typeof data.time === "undefined") {
-			ret_data.status = false;
-			message.push("Time missing");
-		} else {
-			if(data.time.trim() == "") {
-				ret_data.status = false;
-				message.push("Time should not be empty");				
-			}
-			// if (isNaN(data.time)) {
-			// 	ret_data.status = false;
-			// 	message.push("Time should be integer");
-			// }
-		}
+		// if(typeof data.time === "undefined") {
+		// 	ret_data.status = false;
+		// 	message.push("Time missing");
+		// } else {
+		// 	if(data.time.trim() == "") {
+		// 		ret_data.status = false;
+		// 		message.push("Time should not be empty");				
+		// 	}
+		// 	if (isNaN(data.time)) {
+		// 		ret_data.status = false;
+		// 		message.push("Time should be integer");
+		// 	}
+		// }
 
 		if(ret_data.status) {
 			message=[];
