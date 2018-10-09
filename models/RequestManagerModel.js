08bb78223dd9d51b693f0c52dcec53fff8c6349e
Anubhav <anubhav.psit.2009@gmail.com>
2018-10-09 17:51:24 +0530

    saving request url also in request manager

--- models/RequestManagerModel.js
+++ models/RequestManagerModel.js
@@ -9,12 +9,13 @@ function saveRequestRawData(data, callback) {
         data.instance_id,
         data.headers,
         data.raw_request,
+        data.request_url,
         "0"
     ];
     //console.dir(postData);
-    db.query('INSERT INTO api_incoming_requests (instance_id,request_headers,raw_request,status) VALUES (?, ?, ?, ?)', postData, function(err, result) {
+    db.query('INSERT INTO api_incoming_requests (instance_id, request_headers, raw_request, request_url, status) VALUES (?, ?, ?, ?, ?)', postData, function(err, result) {
         if (err) {
-            console.log('this.sql', this.sql);
+            //console.log('this.sql', this.sql);
             throw err;
             console.dir("Error in capturing the request");
         } else {
